b72c04ba8699ff7f9da64be378e1646f8e686394
Juliano Brasil <juliano.brasil@gmail.com>
2019-11-14 07:56:49 -0300

    continuando...

--- DatingApp-SAP/src/app/app.module.ts
+++ DatingApp-SAP/src/app/app.module.ts
@@ -2,6 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 
 import {AppComponent} from './app.component';
 import {NavComponent} from './nav/nav.component';
@@ -10,8 +12,19 @@ import {HomeComponent} from './home/home.component';
 import {ErrorInterceptorProvider} from './_services/error.interceptor';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, RegisterComponent, HomeComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    RegisterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+  ],
   providers: [ErrorInterceptorProvider],
   bootstrap: [AppComponent],
 })
--- DatingApp-SAP/src/app/nav/nav.component.ts
+++ DatingApp-SAP/src/app/nav/nav.component.ts
@@ -8,7 +8,7 @@ import {AuthService} from '../_services/auth.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor(private authService: AuthService) {}
+  constructor(public authService: AuthService) {}
 
   ngOnInit() {}
 
@@ -24,8 +24,7 @@ export class NavComponent implements OnInit {
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
