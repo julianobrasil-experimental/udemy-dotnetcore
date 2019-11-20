5967b9693b05f8cecdb4d822165cb624438ae6d2
Juliano <juliano.brasil@gmail.com>
2019-11-20 11:03:02 -0300

    End of section 6 (#6)
    * End of section 6

--- DatingApp-SAP/src/app/app.module.ts
+++ DatingApp-SAP/src/app/app.module.ts
@@ -2,16 +2,29 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 
-import {AppComponent} from './app.component';
 import {NavComponent} from './nav/nav.component';
 import {RegisterComponent} from './register/register.component';
 import {HomeComponent} from './home/home.component';
 import {ErrorInterceptorProvider} from './_services/error.interceptor';
+import {AppComponent} from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, RegisterComponent, HomeComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    RegisterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+  ],
   providers: [ErrorInterceptorProvider],
   bootstrap: [AppComponent],
 })
